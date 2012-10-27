71c910e2cc15552133016d55c042acff1a74d6e1
Eric LaForce <eric@skookum.com>
2012-10-27 14:48:38 -0400

    Added solution back in.

---
+++ NHibernateRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NHibernateRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ NHibernateRepository/Repository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace NHibernateRepository
+{
+    public abstract class Repository<T> where T : class 
+    {
+        protected readonly ISession _session;
+
+        protected Repository(ISession session)
+        {
+            _session = session;
+            _session.FlushMode = FlushMode.Auto;
+        }
+
+        public bool Add(T entity)
+        {
+            _session.Save(entity);
+            return true;
+        }
+
+        public bool Add(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                _session.Save(item);
+            }
+            return true;
+        }
+
+        public bool Update(T entity)
+        {
+            _session.Update(entity);
+            return true;
+        }
+
+        public bool Delete(T entity)
+        {
+            _session.Delete(entity);
+            return true;
+        }
+
+        public bool Delete(IEnumerable<T> entities)
+        {
+            foreach (T entity in entities)
+            {
+                Delete(entity);
+            }
+            return true;
+        }
+
+        public virtual T GetById(Guid? id)
+        {
+            return _session.Get<T>(id);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _session.Query<T>();
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            return GetMany(expression).SingleOrDefault();
+        }
+
+        public IQueryable<T> GetMany(Expression<Func<T, bool>> expression)
+        {
+            return GetAll().Where(expression).AsQueryable();
+        }
+    }
+}
